176d25dcff0e07f146acc3adf718f063787e3bdd
odiasesamuel <odiasesamuel4@gmail.com>
2024-12-10 16:00:34 +0100

    include trim to schema validation

--- src/schemas/landlordSchema.ts
+++ src/schemas/landlordSchema.ts
@@ -1,20 +1,20 @@
 import { z } from "zod";
 
 export const createLandlordSchema = z.object({
-  firstName: z.string().min(1, "First name is required"),
-  lastName: z.string().min(1, " Last name is required"),
-  email: z.string().email("Invalid email address"),
-  password: z.string().min(6, "Password must be at least 6 characters"),
+  firstName: z.string().trim().min(1, "First name is required"),
+  lastName: z.string().trim().min(1, " Last name is required"),
+  email: z.string().trim().email("Invalid email address"),
+  password: z.string().trim().min(6, "Password must be at least 6 characters"),
 });
 
 export const addPropertySchema = z.object({
-  name: z.string().min(1, "Name of property is required"),
-  email: z.string().email("Invalid email address"),
-  phoneNumber: z.string().min(10, "Phone number is too short").max(15, "Phone number is too long"),
-  description: z.string().min(10, "Description of the property is too short"),
+  name: z.string().trim().min(1, "Name of property is required"),
+  email: z.string().trim().email("Invalid email address"),
+  phoneNumber: z.string().trim().min(10, "Phone number is too short").max(15, "Phone number is too long"),
+  description: z.string().trim().min(10, "Description of the property is too short"),
   propertyDetails: z.object({
-    city: z.string().min(1, "City where the property is located required"),
-    area: z.string().min(1, "Area where the property is located required"),
+    city: z.string().trim().min(1, "City where the property is located required"),
+    area: z.string().trim().min(1, "Area where the property is located required"),
     numberOfRooms: z.number().int().min(1, { message: "The property must have at least 1 room." }),
   }),
 });
