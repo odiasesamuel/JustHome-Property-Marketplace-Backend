60a0f5347b14dd6c1206c9c03c284e7d98316169
odiasesamuel <odiasesamuel4@gmail.com>
2024-12-19 13:38:16 +0100

    implement password encryption

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,6 +3,7 @@ import User from "../models/UserModel";
 import { GlobalErrorHandlerType } from "./errorController";
 import { createUserSchema } from "../schemas/userSchema";
 import { formatValidationError } from "../utils/formatValidationError";
+import bcrypt from "bcryptjs";
 
 export const signup = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -16,11 +17,14 @@ export const signup = async (req: Request, res: Response, next: NextFunction) =>
       throw error;
     }
 
-    const user = new User(validatedData.data);
+    const password = validatedData.data.password;
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const userData = { ...validatedData.data, password: hashedPassword };
+    const user = new User(userData);
     const registeredUser = await user.save();
-    const { firstName, lastName, email, accountType } = registeredUser;
+    const { firstName, lastName, email, accountType, id } = registeredUser;
 
-    res.status(201).json({ message: `${accountType} account has been created`, data: { firstName, lastName, email, accountType } });
+    res.status(201).json({ message: `${accountType} account has been created`, data: { userId: id, firstName, lastName, email, accountType } });
   } catch (error) {
     next(error);
   }
