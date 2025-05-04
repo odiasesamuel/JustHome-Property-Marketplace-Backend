d05de8b663d0aea9e56474f9e26cb7b472aea068
odiasesamuel <odiasesamuel4@gmail.com>
2025-05-04 18:35:36 +0100

    implement github actions to automatically build image and deploy to render

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 import { connectToDatabase } from "./config/db.config";
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 connectToDatabase(() => {
   app.listen(PORT, () => {
